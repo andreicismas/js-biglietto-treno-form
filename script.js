0d426b6a171464e077177b8f0cf8ad02466b74c6
andreicismas <andrei_cismas@yahoo.it>
2021-04-18 23:55:40 +0200

    js cancel btn

--- script.js
+++ script.js
@@ -1,42 +1,73 @@
 
 var buttonGenera = document.getElementById('btn_js_script');
 buttonGenera.addEventListener('click',
-function() {
-  var nomeInput = document.getElementById('nome');
-  var nome = nomeInput.value;
+    function () {
+
+        var show = document.getElementById("show_container");
+        show.style.opacity = "1"
+
+        var nomeInput = document.getElementById('nome');
+        var nome = nomeInput.value;
 
 
-  var kmInput = document.getElementById('distanza_km');
-  var numeroChilometri = kmInput.value;
+        var kmInput = document.getElementById('distanza_km');
+        var numeroChilometri = parseInt(kmInput.value);
 
 
-  var etaFascia = document.getElementById('fascia_eta');
-  var fascia = etaFascia.value;
+        var etaFascia = document.getElementById('fascia_eta');
+        var fascia = etaFascia.value;
 
 
-  var costoChilometro = 0.21;
-  var costoViaggio = numeroChilometri * costoChilometro;
+        var costoChilometro = 0.21;
+        var costoViaggio = numeroChilometri * costoChilometro;
 
 
-  var offerta = 'Tariffa standard';
-  var scontoPercentuale = 0;
+        var offerta = 'Tariffa standard';
 
- 
+        var costoFinale;
 
-  var calcoloSconto = costoViaggio * scontoPercentuale / 100;
-  var costoFinale = costoViaggio - calcoloSconto;
+        if (fascia == "minorenne") {
+            costoFinale = (costoViaggio / 100) * 80;
+            offerta = "Sconto Minorenne";
+        } else if (fascia == "Over65") {
+            costoFinale = (costoViaggio / 100) * 60;
+            offerta = "Sconto maggiorenne";
+        } else {
+            costoFinale = costoViaggio
+        }
 
-  var carrozza = Math.floor(Math.random() * 9) + 1;
-  var codice = Math.floor(Math.random() * (100000 - 90000 + 1) ) + 90000;
-
-  document.getElementById('nome_passeggero').innerHTML = nome;
-  document.getElementById('offerta').innerHTML = offerta;
-  document.getElementById('carrozza').innerHTML = carrozza;
-  document.getElementById('codice').innerHTML = codice;
-  document.getElementById('costo').innerHTML = costoFinale.toFixed(2) + " €";
-
-
-});
+        console.log(fascia)
+        console.log(costoViaggio)
+        console.log(costoFinale)
 
 
+
+        var carrozza = Math.floor(Math.random() * 9) + 1;
+        var codice = Math.floor(Math.random() * (100000 - 90000 + 1)) + 90000;
+
+
+        document.getElementById('nome_passeggero').innerHTML = nome;
+        document.getElementById('offerta').innerHTML = offerta;
+        document.getElementById('carrozza').innerHTML = carrozza;
+        document.getElementById('codice').innerHTML = codice;
+        document.getElementById('costo').innerHTML = costoFinale.toFixed(2) + " €";
+
+    });
+
+
+var btnAnnulla = document.getElementById('cancel_ticket');
+btnAnnulla.addEventListener('click',
+    function () {
+
+        var show = document.getElementById("show_container");
+        show.style.opacity = "0 "
+         
+
+     
+        var nomeInput = document.getElementById('nome').value ="";
+        var kmInput = document.getElementById('distanza_km').value ="";
+        var etaFascia = document.getElementById('fascia_eta').value ="";
+
+    }
+);
 
